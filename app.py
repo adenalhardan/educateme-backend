b3266d54ca8c5f50411fd60462572ed9b6f01c34
Aden  Al-Hardan <adenalhardan@gmail.com>
2023-04-11 21:57:28 -0400

    RDS

--- app.py
+++ app.py
@@ -1,9 +1,32 @@
 from fastapi import FastAPI
 from mangum import Mangum
+import boto3
+import os
 
 app = FastAPI()
 handler = Mangum(app)
+rds_client = boto3.client('rds-data', region_name = 'us-west-1')
+
+def execute(sql, type, args = []):
+    response = rds_client.execute_statement(
+        secretArn = os.environ.get('db_credentials_secrets_store_arn'),
+        database = os.environ.get('database_name'),
+        resourceArn = os.environ.get('db_cluster_arn'),
+        sql = sql,
+        parameters = args
+    )
+    
+    if type in ['POST', 'UPDATE', 'DELETE']:
+        if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+            return {'status': 'success'}  
+        else:
+            return {'status': 'error', 'message': 'could modify into database'}
+
+    elif type == 'GET':
+        return response['records']
+
+    return {'status': 'error', 'message': 'invalid type'}
 
 @app.get('/')
 async def root():
-    return {'message': 'educate me'}
+    return {'message': execute('SELECT * FROM educateme.users', 'GET')}
